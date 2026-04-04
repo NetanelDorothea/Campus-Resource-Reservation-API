63d32146178b0e7e263a7090b72192236a0d113b
Netanel Dorothea <netaneldorothea@hotmail.com>
2026-04-04 10:07:45 -0400

    Error Handling and Logging | Part A

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const express = require('express');
  
+const requestLogger = require('./middleware/requestLogger');
 const usersRoutes = require('./routes/users');
 const resourcesRoutes = require('./routes/resources');
 const reservationsRoutes = require('./routes/reservations');
 const authRoutes = require('./routes/auth')
 const errorHandler = require('./middleware/errorHandler');
+
  
 const app = express();
  
 app.use(express.json());
  
+app.use(requestLogger);
 app.use('/api/users', usersRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/resources', resourcesRoutes);
